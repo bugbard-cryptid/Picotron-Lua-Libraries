60fabb3cf31b0efcf6cb65f90f91c9f394ab1c6c
wren <wilrnilr@gmail.com>
2025-07-14 22:23:28 -0700

    update dialogue_script.lua

--- src/dialogue/dialogue_script.lua
+++ src/dialogue/dialogue_script.lua
@@ -1,48 +1,53 @@
---[[pod_format="raw",created="2025-07-14 23:40:00",modified="2025-07-15 01:07:58",revision=48]]
--- here is an example script for a dialogue!
--- try making a new dialogue and using this as the script, like this:
---[[
-
-my_dialogue = dialogue.new{
-	script = include("dialogue_script.lua")
-}
-
-]]
-
-
+--[[pod_format="raw",created="2025-07-14 23:40:00",modified="2025-07-15 05:13:42",revision=68]]
 local script = {
-	{text="choices with seeking"},
-
-	"query",
-	{
-		text="pick one of these choices ok",
-		choices = {
-			{text="choice 1 (correct)", func = function (self)
-				self:advance("correct")
-			end},
-			{text="choice 2 (wrong)", func = function (self)
-				self:advance("wrong")
-			end},
-			{text="choice 3 (go to start)", func = function (self)
-				self:advance(1, true)
-			end},
+	["intro"] = {
+		{text="choices with seeking"},
+		"query"
+	},
+	
+	["query"] = {
+		{
+			text="pick one of these choices ok",
+			choices = {
+				{text="choice 1 (correct)", 		result = "correct"},
+				{text="choice 2 (wrong)", 			result = "wrong"},
+				{text="choice 3 (go to start)", 	result = "intro"},
+			},
 		},
 	},
 
-	"correct",
-	{text="you selected 1!"},
-	{text="that is the \fccorrect one\f7!"},
-	function (self) self:advance("end") end,
+	["correct"] = {
+		{text="you selected 1!"},
+		{text="that is the \fccorrect one\f7!"},
+		"end",
+	},
 	
-	"wrong",
-	{text="you selected 2!"},
-	{text="that is the \fuwrong one\f7!"},
-	function (self) self.box = false self:advance(1) end,
-	{text="because you selected wrong,\ni am taking your box away"},
-	function (self) self:advance("query") end,
+	["wrong"] = {
+		{text="you selected 2!"},
+		{text="that is the \fuwrong one\f7!"},
+		function (self)
+			if self.box then
+				self.box = false
+				self:advance("take_box")
+			else
+				self:advance("no_box")
+			end
+		end,
+	},
 	
-	"end",
-	{text = "the end!"},
+	["take_box"] = {
+		{text="because you selected wrong,\ni am taking your box away"},
+		"query",
+	},
+	
+	["no_box"] = {
+		{text="loser"},
+		"query",
+	},
+	
+	["end"] = {
+		{text = "the end!"},
+	}
 }
 
 return script
